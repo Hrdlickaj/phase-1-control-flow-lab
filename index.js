7d388f07a950daaa453ce2271c73af0c4d2b2765
Jenna Hrdlicka <hrdlickajenna@gmail.com>
2022-04-06 10:04:41 -0600

    commit tree b9ce0730d86832b77195be54a0618ccaebc0a47d

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(depthInFeet){
+    if (depthInFeet <= 400) {
+    return 'This one is on me!'; }
+    else if (depthInFeet <= 2000) {
+    return 'That will be twenty bucks.'; }
+    else if (depthInFeet <= 2500) {
+    return 'I will gladly take your thirty bucks.'; }
+    else {
+    return 'No can do.'; }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+    let cityMessage = (city === 'NYC' ? 'Ok, sounds good.' : "No go.")
+    return cityMessage
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+    switch (tip) {
+    case 'generous':
+    return ('Thank you so much.');
+    case 'not as generous':
+    return ('Thank you.');
+    default: 
+    return ('Bye.');
+    }
 }
